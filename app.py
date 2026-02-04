337c943442a2ab407a0c5ad99e15cdf26eb2e318
krishna patel <kinsu2@users.noreply.huggingface.co>
2026-02-04 12:15:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ model.load_state_dict(torch.load(weights_path,map_location="cpu"))
 model.eval()
 #preprocessing:same as training
 IMG_HEIGHT,IMG_WIDTH=128,128
-transform=transform.compose([
+transform=transform.Compose([
     transform.resize((IMG_HEIGHT,IMG_WIDTH)),
     transforms.ToTensor()
 ])
@@ -24,12 +24,14 @@ def predict(image):
     orig_w,orig_h=image.size#originalsize of uploaded image
     img=transform(image).unsqueeze(0)#(1,3,128,128)
     with torch.no_grad():
-        pred_model(img)
+         pred=model(img)
+        
     mask =pred.squeeze(0).squeeze(0).cpu().numpy()
-    mask=(mask*255).astype(np.unit8)#grayscale
+    mask=(mask*255).astype(np.uint8)#grayscale mask
+    
     #resize  back to original size
-    mask_img=Image.fromarray(mask),resize((orig_h),Image.NEAREST)
-    retrun mask_img
+    mask_img=Image.fromarray(mask).resize((orig_w,orig_h),Image.NEAREST)
+    return mask_img
 #gradio interface
 demo=gr.Interface(
     fn=predict,
