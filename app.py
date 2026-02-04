c0163daa08567c203d67dd379da8dd985362ed1c
krishna patel <kinsu2@users.noreply.huggingface.co>
2026-02-04 13:14:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from huggingface_hub import hf_hub_download
 
 
 weights_path = hf_hub_download(
-    repo_id="kinsu2/unet-for-crack-concrete",
+    repo_id="kinsu2/concrete-crack",
     filename="unet_model.pth"    
 )
 
